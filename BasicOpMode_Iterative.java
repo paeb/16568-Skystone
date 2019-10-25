94b2a4dc5e642690281e98ab777910c6086ab5ca
Andrew-Ogundimu <56798377+Andrew-Ogundimu@users.noreply.github.com>
2019-10-25 17:34:01 -0400

    Add files via upload
    This version of Basic Op Mode Iterative has Servos

--- BasicOpMode_Iterative.java
+++ BasicOpMode_Iterative.java
@@ -1,19 +1,41 @@
-/* Written by Lawson Wright and Andrew Ogundimu
- * Created on: 10/20/19
- * Last updates on: 10/20/19
- * Modified version of the BasicOpMode_Iterative external sample.
- * Discription: This is a baisc teleop code used for control of a robot with a gamepad. It has basic movement and turning.
- * It also has speed control with the dpad.
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.hardware.Servo;
 
 /**
  * This file contains an example of an iterative (Non-Linear) "OpMode".
@@ -35,40 +57,41 @@ public class BasicOpMode_Iterative extends OpMode
 {
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor backLeftDrive = null; //Port: 1
-    private DcMotor frontLeftDrive = null; //Port: 3
-    private DcMotor backRightDrive = null; //Port: 0
-    private DcMotor frontRightDrive = null; //Port: 2
-    public Servo testServo = null;
-
-
-    private double speedFactor = 5;
+    private DcMotor frontLeftDrive = null;
+    private DcMotor frontRightDrive = null;
+    private DcMotor backLeftDrive = null;
+    private DcMotor backRightDrive = null;
     private boolean lastPressed = false;
-
-
+    private double speedFactor = 1;
+    private Servo servo = null;
     /*
      * Code to run ONCE when the driver hits INIT
      */
     @Override
     public void init() {
         telemetry.addData("Status", "Initialized");
-
+        /*
+        back right = port 0
+        back left = port 1
+        front right = port 2??
+        front left = port 3??
+         */
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration
         // step (using the FTC Robot Controller app on the phone).
         frontLeftDrive  = hardwareMap.get(DcMotor.class, "front_left_drive");
-        backLeftDrive  = hardwareMap.get(DcMotor.class, "back_left_drive");
         frontRightDrive = hardwareMap.get(DcMotor.class, "front_right_drive");
+        backLeftDrive = hardwareMap.get(DcMotor.class, "back_left_drive");
         backRightDrive = hardwareMap.get(DcMotor.class, "back_right_drive");
-        testServo = hardwareMap.get(Servo.class, "testServo");
-
+        servo = hardwareMap.get(Servo.class, "testServo");
+        servo.setPosition(0.0);
+        //servo.setPosition(0);
         // Most robots need the motor on one side to be reversed to drive forward
         // Reverse the motor that runs backwards when connected directly to the battery
         frontLeftDrive.setDirection(DcMotor.Direction.FORWARD);
-        backLeftDrive.setDirection(DcMotor.Direction.FORWARD);
         frontRightDrive.setDirection(DcMotor.Direction.REVERSE);
+        backLeftDrive.setDirection(DcMotor.Direction.FORWARD);
         backRightDrive.setDirection(DcMotor.Direction.REVERSE);
-
         // Tell the driver that initialization is complete.
         telemetry.addData("Status", "Initialized");
     }
@@ -103,38 +126,34 @@ public class BasicOpMode_Iterative extends OpMode
         // POV Mode uses left stick to go forward, and right stick to turn.
         // - This uses basic math to combine motions and is easier to drive straight.
         double drive = -gamepad1.left_stick_y;
-        double turn  =  gamepad1.right_stick_x;
-        if ((gamepad1.dpad_up) && (speedFactor <= .9) && (gamepad1.dpad_up != lastPressed)){
-            speedFactor += .1;
-        } else if ((gamepad1.dpad_down) && (speedFactor >= .2) && (!(gamepad1.dpad_up == lastPressed))){
+        double turn  =  -gamepad1.left_stick_x;
+        leftPower    = Range.clip((drive + turn)*speedFactor, -1.0, 1.0) ;
+        rightPower   = Range.clip((drive - turn)*speedFactor, -1.0, 1.0) ;
+        if (gamepad1.dpad_down && speedFactor>=0.2 && lastPressed!=gamepad1.dpad_down) {
             speedFactor -= .1;
+        } else if (gamepad1.dpad_up && speedFactor<=0.9 && lastPressed!=gamepad1.dpad_up) {
+            speedFactor += .1;
         }
-        if (gamepad1.a){
-            testServo.setPosition(.3);
-        } else if (gamepad1.b){
-            testServo.setPosition(.7);
-        }
-        leftPower    = Range.clip((drive + turn) * speedFactor, -1.0, 1.0) ;
-        rightPower   = Range.clip((drive - turn) * speedFactor, -1.0, 1.0) ;
 
         // Tank Mode uses one stick to control each wheel.
         // - This requires no math, but it is hard to drive forward slowly and keep straight.
         // leftPower  = -gamepad1.left_stick_y ;
         // rightPower = -gamepad1.right_stick_y ;
-
+        if (gamepad1.a) {
+            servo.setPosition(0.3);
+        } else if (gamepad1.b) {
+            servo.setPosition(0.7);
+        }
         // Send calculated power to wheels
         frontLeftDrive.setPower(leftPower);
-        backLeftDrive.setPower(leftPower);
         frontRightDrive.setPower(rightPower);
+        backLeftDrive.setPower(leftPower);
         backRightDrive.setPower(rightPower);
-
         // Show the elapsed game time and wheel power.
         telemetry.addData("Status", "Run Time: " + runtime.toString());
-        telemetry.addData("A and B button","Button Press: A:"+gamepad1.a+" B; "+gamepad1.b);
-        telemetry.addData("SpeedFactor", "Speed Factor Value: " +  Math.round(speedFactor*10));
+        telemetry.addData("Speed Factor", Math.round(speedFactor*10));
+        telemetry.addData("Servo: ",servo.getPosition());
         telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
-        telemetry.update();
-
         lastPressed = gamepad1.dpad_up || gamepad1.dpad_down;
     }
 
