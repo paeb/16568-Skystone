26c205cb20fb34c06ded3feaee72135aa4998de8
Lawson-Wright <56798372+Lawson-Wright@users.noreply.github.com>
2019-11-03 11:01:12 -0500

    Update BasicOpMode_Iterative_Strafe.java

--- BasicOpMode_Iterative_Strafe.java
+++ BasicOpMode_Iterative_Strafe.java
@@ -54,20 +54,12 @@ public class BasicOpMode_Iterative_Strafe extends OpMode
     public void init() {
         telemetry.addData("Status", "Initializing");
 
-        // Initialize the hardware variables. Note that the strings used here as parameters
-        // to 'get' must correspond to the names assigned during the robot configuration
-        // step (using the FTC Robot Controller app on the phone).
-
         //Drive declaration
         frontLeftDrive  = hardwareMap.get(DcMotor.class, "front_left_drive");
         frontRightDrive = hardwareMap.get(DcMotor.class, "front_right_drive");
         backLeftDrive = hardwareMap.get(DcMotor.class, "back_left_drive");
         backRightDrive = hardwareMap.get(DcMotor.class, "back_right_drive");
 
-
-
-        // Most robots need the motor on one side to be reversed to drive forward
-        // Reverse the motor that runs backwards when connected directly to the battery
 
         //Drive settings
         frontLeftDrive.setDirection(DcMotor.Direction.FORWARD);
